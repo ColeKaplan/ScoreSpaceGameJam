654ab6267fd9c75bcbf520f966eb59ca900b5be9
jackwarren430 <90930942+jackwarren430@users.noreply.github.com>
2023-06-16 17:50:48 -0400

    minor changes to script

--- Assets/Scripts/hero_behavior.cs
+++ Assets/Scripts/hero_behavior.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class hero_behavior : MonoBehaviour
 {
+
+    private bool inPlay;
+    private HeroState heroState;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        inPlay = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (inPlay)
+        {
+            switch (heroState)
+            {
+                case HeroState.Walking:
+                    break;
+                case HeroState.WalkingDown:
+                    break;
+                case HeroState.WalkingUp:
+                    break;
+                
+            }
+        }
     }
+
+    private enum HeroState
+    {
+        Walking,
+        WalkingDown,
+        WalkingUp,
+        
+
+    }
+
 }
