da292b886bd328a5a89917bab9ac1f5c5baa5913
jackwarren430 <90930942+jackwarren430@users.noreply.github.com>
2023-06-16 20:45:16 -0400

    added repeating background

---
+++ Assets/Scripts/background_scroll.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background_scroll : MonoBehaviour
+{
+
+    public GameObject camera;
+    private float backgroundWidth;
+    
+    void Start()
+    {
+        backgroundWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    
+    void Update()
+    {
+        if (transform.position.x + backgroundWidth < camera.transform.position.x)
+        {
+            Vector2 newPos = new Vector2(transform.position.x + 2 * backgroundWidth - 0.1f, transform.position.y);
+            transform.position = newPos;
+        }
+    }
+}
