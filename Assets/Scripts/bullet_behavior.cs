55efd8252e91a619ae0986dfbd18dda0d5034373
Cole Kaplan <44578832+ColeKaplan@users.noreply.github.com>
2023-06-16 23:59:19 -0400

    Bullet stuff
    yuh bullet hat

--- Assets/Scripts/bullet_behavior.cs
+++ Assets/Scripts/bullet_behavior.cs
@@ -5,23 +5,39 @@ using UnityEngine;
 public class bullet_behavior : MonoBehaviour
 {
     
-    private float speed = 6f;
+    private float speed = 8;
     public GameObject camera;
+    private float spinningInterval = .1f;
+    private float timer = 0f; // Timer to track the elapsed time
 
     void Start()
     {
-        
+        //gameObject.GetComponent<Rigidbody>().velocity = Vector3.right * speed;
     }
 
     
     void Update()
     {
-        if (!IsVisible(gameObject, camera.GetComponent<Camera>()))
+        gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.right * speed;
+
+
+        timer += Time.deltaTime; // Increase the timer by the elapsed time
+
+        if (timer >= spinningInterval)
+        {
+            Spin();
+            timer = 0f; // Reset the timer after shooting
+            
+        }
+
+        /*if (!IsVisible(gameObject, camera.GetComponent<Camera>()))
         {
             Debug.Log("bullet destroyed - camera");
             Destroy(gameObject);
-        }
-        transform.Translate(new Vector2(speed * Time.deltaTime, 0));
+        }*/
+        //transform.Translate(new Vector2(speed * Time.deltaTime, 0));
+        
+
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -37,5 +53,10 @@ public class bullet_behavior : MonoBehaviour
     {
         Vector3 viewportPoint = camera.WorldToViewportPoint(gameObject.transform.position);
         return viewportPoint.z > 0 && viewportPoint.x > 0 && viewportPoint.x < 1 && viewportPoint.y > 0 && viewportPoint.y < 1;
+    }
+
+    void Spin()
+    {
+        this.transform.Rotate(0f, 0f, 45f);
     }
 }
